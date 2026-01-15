6fb6f19e308fa0cd0d9bc48cd082233d83b8e920
Sailen-Mondal <mr.sailenmondal@gmail.com>
2026-01-15 15:03:02 +0530

    Adding Stack Folder

--- Stack/ImplimentStackUsingList.java
+++ Stack/ImplimentStackUsingList.java
@@ -1,4 +1,4 @@
-package LinkedList;
+package Stack;
 
 public class ImplimentStackUsingList
 {
@@ -20,15 +20,21 @@ public class ImplimentStackUsingList
     //Implimenting pop
     int pop()
     {
+        if(head == null)
+            return -1;
+        
         int poped = head.data;
         head = head.next;
         return poped;
     }
     
     //peek
-    void peek()
+    int peek()
     {
-        System.out.println("Stack top: " + head.data);
+        if(head == null)
+            return -1;
+
+        return head.data;
     }
 
 
@@ -38,10 +44,19 @@ public class ImplimentStackUsingList
         stack.push(10);
         stack.push(20);
         stack.push(30);
+        stack.push(40);
+        stack.push(50);
 
-        stack.peek();
+        System.out.println("Peek: " + stack.peek());
 
         int poppedData = stack.pop();
         System.out.println("Popped: " + poppedData);
+
+        //Printing the stack
+        while(stack.head != null)
+        {
+            System.out.println(stack.peek());
+            stack.pop();
+        }
     }
 }
