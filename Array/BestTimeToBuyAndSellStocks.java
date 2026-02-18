206e0003ddacd41231cfd73499d16781b4affe21
Sailen-Mondal <mr.sailenmondal@gmail.com>
2026-02-18 13:45:44 +0530

    Adding new file

---
+++ Array/BestTimeToBuyAndSellStocks.java
@@ -0,0 +1,34 @@
+package Array;
+
+public class BestTimeToBuyAndSellStocks {       //Bruteforce Method
+
+    public static void maxProfit(int[] prices)
+    {
+        int maxProfit = 0;
+
+        int buyDay = 0;
+        int sellDay = 0;
+
+        for(int k = 1; k < prices.length; k++)  //Dynamic sliding window
+        {
+            for(int i = 0; i+k < prices.length; i++)
+            {
+                int profit = prices[i+k] - prices[i];
+
+                if(profit > maxProfit)
+                {
+                    maxProfit = profit;
+                    buyDay = i + 1;
+                    sellDay = i + k + 1;
+                }
+            }
+        }
+        System.out.println("Profit: " + maxProfit);
+        System.out.println("Buy at Day: " + buyDay + "; Sell at day: " + sellDay);
+    }
+    
+    public static void main(String[] args) {
+        int prices[] = {7,1,5,3,6,4};
+        maxProfit(prices);
+    }
+}
