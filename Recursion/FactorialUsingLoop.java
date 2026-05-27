c7e574b8867a82458f0c1833574ba1ff0b330f64
Sailen Mondal <mr.sailenmondal@gmail.com>
2026-05-27 21:43:53 +0530

    same

--- Recursion/FactorialUsingLoop.java
+++ Recursion/FactorialUsingLoop.java
@@ -13,6 +13,7 @@ public class FactorialUsingLoop{
 	
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number: ");
 		int num = sc.nextInt();
 		System.out.println("Factorial: " + returnFactorial(num));
 		sc.close();
