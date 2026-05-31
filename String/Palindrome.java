3bcb19a28c32389494b096b47aff99599c458855
Sailen Mondal <mr.sailenmondal@gmail.com>
2026-05-31 21:44:23 +0530

    Palindrome file

---
+++ String/Palindrome.java
@@ -0,0 +1,33 @@
+package String;
+
+import java.util.*;
+
+public class Palindrome {
+
+    public static boolean palindromeCheck(String str) {
+        int i = 0;
+        int j = str.length() - 1;
+
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        System.out.println(palindromeCheck(str));
+
+        sc.close();
+    }
+}
