2e0759bdb436ce37274c8a27d3fcdd69a98a66b8
Sailen Mondal <mr.sailenmondal@gmail.com>
2026-06-16 19:07:01 +0530

    new method added for reverse a string

--- String/ReverseString.java
+++ String/ReverseString.java
@@ -1,6 +1,13 @@
 
 
 public class ReverseString {
+    public String revString(String str){
+        StringBuilder sb = new StringBuilder(str);
+        sb.reverse();
+        str = sb.toString();
+        return str;
+    }
+
     public static void main(String[] args) {
         String name = "Sailen Mondal";
         String revName = new String(); 
@@ -10,5 +17,8 @@ public class ReverseString {
             revName = revName + name.charAt(i);     //Space complixity is fucked!(New memory for each iteration)
         }
         System.out.println(revName);
+        ReverseString obj = new ReverseString();
+        name = obj.revString(name);    //Better Approach!
+        System.out.println(name);
     }
 }
