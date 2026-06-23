fd4690d404942940843e23b912ba05acbb6652be
Sailen Mondal <mr.sailenmondal@gmail.com>
2026-06-23 19:50:29 +0530

    Adding new file

---
+++ Recursion/FindSum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class FindSum {
+
+    public static int findSum(int n){
+    if( n == 0) return 0;
+    
+    return n + findSum(n - 1);
+}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+        int sum = findSum(n);
+        System.out.println(sum);
+
+        sc.close();
+    }
+} 
